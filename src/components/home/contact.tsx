d9a16076ae886414bb9b12ccd4df9dea0ebb3457
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-08-02 18:27:29 +0100

    started mobile

--- src/components/home/contact.tsx
+++ src/components/home/contact.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div className="flex flex-col">
-        <div className="bg-primary w-fit py-24 rounded-b-[30px]">
+        <div className="bg-primary w-fit py-12 md:py-24 rounded-b-[30px]">
           <Image
             src={"/images/logo.png"}
             className="w-[150px] h-[126px]"
@@ -17,17 +17,17 @@ const Contact = () => {
             height={1000}
           />
         </div>
-        <h1 className="text-primary font-[600] text-6xl mt-14 text-balance">
+        <h1 className="text-primary font-[600] text-3xl md:text-6xl mt-7 md:mt-14 text-balance">
           Connect with me for more information
         </h1>
-        <p className="text-[#3B3B3B] text-xl mt-6 text-balance">
+        <p className="text-[#3B3B3B] md:text-xl mt-3 md:mt-6 text-balance">
           Your information will be kept private and confidential and will never
           be sold or shared with third-parties
         </p>
       </div>
 
-      <div className="py-12">
-        <div className="space-y-8 bg-[#F8F8F8] rounded-[30px] p-[50px]">
+      <div className="py-4 md:py-12">
+        <div className="space-y-8 bg-[#F8F8F8] rounded-[30px] p-6 md:p-[50px]">
           <div className="space-y-4">
             <Label>Your Name</Label>
             <Input
