c751a36c323a1fa2361e252a01def27b8fd78ae2
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-08-02 11:17:29 +0100

    added services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,21 @@
+import About from "@/components/about/about";
+import Hero from "@/components/services/hero";
+import Contact from "@/components/home/contact";
+import Footer from "@/components/shared/footer";
+import Nav from "@/components/shared/nav";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+      <div className="mx-auto max-w-[1440px] px-[16px] md:px-[100px]">
+        <About />
+      </div>
+      <div className="mx-auto max-w-[1440px] px-[16px] md:px-[100px]">
+        <Contact />
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/services/hero.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 h-[100vh] bg-black/75 services-background">
+      <Image src={"/images/logo.png"} alt="" width={100} height={100} />
+      <h1 className="font-[600] text-[60px] text-white text-center">
+        Services That Support Your Individual Journey
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
